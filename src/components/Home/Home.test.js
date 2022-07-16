a8b607de2e10f0f3b2f1353e8afb54c61806901b
David <davidgranados73@gmail.com>
2022-07-15 19:00:39 -0500

    Home test

--- src/components/Home/Home.test.js
+++ src/components/Home/Home.test.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom'
 import {createMemoryHistory} from 'history'
 import {Router} from 'react-router-dom'
 import {Home} from './Home';
+import {deleteNote} from '../../context/noteFirestore'
 import { AuthProvider } from '../../context/authContext.js';
 
 jest.mock("../../context/authContext.js")
@@ -20,7 +22,8 @@ describe.only("prueba del Home", () => {
           </AuthProvider>
         );
         const linkElement = screen.getByTestId("delete-note");
-        console.log("linkElement", linkElement);
         expect(linkElement).toBeInTheDocument();
+        userEvent.click(linkElement);
+        expect(deleteNote).toHaveBeenCalled();
       });
     })
